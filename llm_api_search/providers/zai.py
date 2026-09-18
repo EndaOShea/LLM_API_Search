f694f0ed4a799a10c33a9aa67bd9ba5386700eaf
EndaOShea <17296926+EndaOShea@users.noreply.github.com>
2026-09-18 11:19:40 +0000

    chore: update static model data from live APIs

--- llm_api_search/providers/zai.py
+++ llm_api_search/providers/zai.py
@@ -219,6 +219,19 @@ _STATIC_MODELS = [
         input_cost_per_mtok=0.15,
         output_cost_per_mtok=0.5,
     ),
+    TextModelInfo(
+        model_id='glm-5.3-flashx',
+        display_name='glm-5.3-flashx',
+        description='',
+        context_window=None,
+        max_output_tokens=None,
+        supports_vision=False,
+        supports_tool_use=False,
+        supports_image_generation=False,
+        supports_computer_use=False,
+        input_cost_per_mtok=None,  # TODO: add pricing
+        output_cost_per_mtok=None,  # TODO: add pricing
+    ),
 ]
 
 
